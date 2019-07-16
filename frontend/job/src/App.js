5e696abe610bf4eb97aafb5874539ce63793cdf3
choi94 <ehdgns94@icloud.com>
2019-07-16 17:56:39 +0900

    07_16 commit

---
+++ frontend/job/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Member from './member/Member'
+import NavbarPage from './member/NavbarPage'
+
+const App = () => (
+  <div>
+    <NavbarPage></NavbarPage>
+    <Member></Member>
+  </div>
+)
+
+export default App;
+ 
--- frontend/job/src/index.js
+++ frontend/job/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/index.css';
-import App from './containers/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
