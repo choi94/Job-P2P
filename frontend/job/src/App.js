4fad3de16bdf8ecde874c977dc2d94939236b20d
0505ssk <0505ssk@naver.com>
2019-07-19 08:37:17 +0900

    no message

--- frontend/job/src/App.js
+++ frontend/job/src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import Mynavbar from './mypage/Mynavbar'
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Page from "./mypage/Page"
+import ResPage from "./mypage/side/ResPage"
+import SupPage from "./mypage/side/SupPage"
+import Info from "./mypage/side/Info"
+import InfoUpdate from "./mypage/click/InfoUpdate"
+import Resume from "./mypage/click/Resume"
+import Password from "./mypage/click/Password"
+import Drop from "./mypage/click/Drop"
+import SupMini from "./mypage/click/SupMini"
 
 function App() {
   return (
     <div>
+      <Mynavbar></Mynavbar>
+
+      <Router>
+        <Route path="/page" component={Page}/>
+        <Route path="/respage" component={ResPage}/>
+        <Route path="/suppage" component={SupPage}/>
+        <Route path="/info" component={Info}/>
+        <Route path="/infoupdate" component={InfoUpdate}/>
+        <Route path="/resume" component={Resume}/>
+        <Route path="/password" component={Password}/>
+        <Route path="/drop" component={Drop}/>
+        <Route path="/sup" component={SupMini}/>
+      </Router>
     </div>
   );
 }
