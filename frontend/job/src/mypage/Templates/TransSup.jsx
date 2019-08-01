044780e5a3447aa64d83490a26b890f37396764a
0505ssk <0505ssk@naver.com>
2019-08-01 19:11:42 +0900

    08.01 거래페이지 수정

--- frontend/job/src/mypage/Templates/TransSup.jsx
+++ frontend/job/src/mypage/Templates/TransSup.jsx
@@ -1,40 +1,35 @@
 import React, { useState}from 'react'
-import {Table, Button} from 'react-bootstrap'
+import {ListGroup, Button} from 'react-bootstrap'
+import '../css/index.css'
 
 const TransSup  = (props) => {
     console.dir(props)
     return(
         <div>
-            <Table>
-                <thead>
-                    <tr>
-                        <td className="vol">지원자 정보</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>일자리 제목</th>
-                        <th>을지로 이사 도우미</th>
-                    </tr>
-                    <tr>
-                        <th>기간</th>
-                        <th>2019/07/17 ~ 2019/07/19</th>
-                    </tr>
-                    <tr>
-                        <th>지원자 이름</th>
-                        <th>홍길동</th>
-                    </tr>
-                    <tr>
-                        <th>지원자 연락처</th>
-                        <th>010-0000-0000</th>
-                    </tr>
-                    <tr>
-                        <th>총 급여</th>
-                        <th>24만원</th>
-                    </tr>
-                </tbody>
-            </Table><br/>
-            <Button variant="success">확인</Button>
+            <h2 className="vol">지원자 정보</h2>
+            <ListGroup>
+                <ListGroup.Item className="TransBox1">
+                    <div className="TransBoxTitle">일자리 제목</div>
+                    <div>을지로 </div>
+                </ListGroup.Item>
+                <ListGroup.Item className="TransBox1">
+                    <span className="TransBoxTitle">기간 </span>
+                    <span>2019/07/17 ~ 2019/07/19</span>
+                </ListGroup.Item>
+                <ListGroup.Item className="TransBox1">
+                    <span className="TransBoxTitle">지원자 이름 </span>
+                    <span>홍길동</span>
+                </ListGroup.Item>
+                <ListGroup.Item className="TransBox1">
+                    <span className="TransBoxTitle">지원자 연락처 </span>
+                    <span >010-0000-0000</span>
+                </ListGroup.Item>
+                <ListGroup.Item className="TransBox1">
+                    <span className="TransBoxTitle">총 급여 </span>
+                    <span>24만원</span>
+                </ListGroup.Item>
+            </ListGroup>
+            <Button variant="success" className="TransBoxButton">확인</Button>
         </div>
     );
 }
